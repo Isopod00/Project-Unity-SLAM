c8888a46fd7f9b4ff7b96d5fb11497d62c5cd2bd
Anthony Brogni <anthonybrog@gmail.com>
2023-09-24 20:00:58 -0500

    switch from using occupancygrids to colored pointclouds

--- Assets/Scripts/MapUpdater.cs
+++ Assets/Scripts/MapUpdater.cs
@@ -1,50 +1,54 @@
 using UnityEngine;
-
 using Unity.Robotics.ROSTCPConnector;
-using RosMessageTypes.Nav;
+using RosMessageTypes.Sensor;
+using System.Collections.Generic;
 
-public class MapUpdater : MonoBehaviour
+public class PointCloudUpdater : MonoBehaviour
 {
-    public string mapTopic = "/map"; // Configure the map topic name here
-    private MeshRenderer mapRenderer; // Reference to the mesh renderer for the map
-    private Texture2D mapTexture; // Texture to update with the map data
+    public string pointCloudTopic = "/zed2i/zed_node/point_cloud/cloud_registered"; // Configure the point cloud topic name here
+    private MeshRenderer pointCloudRenderer; // Reference to the mesh renderer for the point cloud
+    private Texture2D pointCloudTexture; // Texture to update with the point cloud data
 
     void Start()
     {
-        mapRenderer = GetComponent<MeshRenderer>(); // Assumes the map is displayed on the same GameObject as this script
+        pointCloudRenderer = GetComponent<MeshRenderer>(); // Assumes the point cloud is displayed on the same GameObject as this script
 
-        mapTexture = new Texture2D(1, 1); // Initialize a blank texture
-        mapRenderer.material.mainTexture = mapTexture; // Assign the texture to the material
+        pointCloudTexture = new Texture2D(1, 1); // Initialize a blank texture
+        pointCloudRenderer.material.mainTexture = pointCloudTexture; // Assign the texture to the material
 
-        ROSConnection.GetOrCreateInstance().Subscribe<OccupancyGridMsg>(mapTopic, MapCallback); // Subscribe to the map topic
+        ROSConnection.GetOrCreateInstance().Subscribe<PointCloud2Msg>(pointCloudTopic, PointCloudCallback); // Subscribe to the point cloud topic
     }
 
-    private void MapCallback(OccupancyGridMsg message)
+    private void PointCloudCallback(PointCloud2Msg message)
     {
-        Debug.Log("MapCallback called");
+        Debug.Log("PointCloudCallback called");
 
-        int width = (int)message.info.width;
-        int height = (int)message.info.height;
+        int width = (int)message.width;
+        int height = (int)message.height;
 
-        // Ensure the map texture size matches the incoming map
-        if (mapTexture.width != width || mapTexture.height != height)
+        // Ensure the point cloud texture size matches the incoming data
+        if (pointCloudTexture.width != width || pointCloudTexture.height != height)
         {
-            mapTexture.Reinitialize(width, height);
+            pointCloudTexture.Reinitialize(width, height);
         }
 
         Color[] colors = new Color[width * height];
+
+        // Iterate through point cloud data and convert it to colors
         for (int i = 0; i < width * height; i++)
         {
-            sbyte mapValue = message.data[i];
-            byte convertedValue = (byte)(mapValue + 128); // Convert sbyte to byte
-            Color pixelColor = (convertedValue == 0) ? Color.black : Color.white; // Modify color based on map data
+            float x = message.data[i * message.point_step + message.fields[0].offset];
+            float y = message.data[i * message.point_step + message.fields[1].offset];
+            float z = message.data[i * message.point_step + message.fields[2].offset];
+
+            // Modify color based on point cloud data (example: map Z values to grayscale)
+            Color pixelColor = new Color(z, z, z);
             colors[i] = pixelColor;
         }
 
-        mapTexture.SetPixels(colors);
-        mapTexture.Apply(); // Apply changes to the texture
+        pointCloudTexture.SetPixels(colors);
+        pointCloudTexture.Apply(); // Apply changes to the texture
 
-        Debug.Log("MapCallback finished");
+        Debug.Log("PointCloudCallback finished");
     }
-
 }
