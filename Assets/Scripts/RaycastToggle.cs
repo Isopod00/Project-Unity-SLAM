e084eced5c5c4e7699a443316b413a075c5d590f
Anthony Brogni <anthonybrog@gmail.com>
2023-10-19 16:36:36 -0500

    Added a movable Marker!

--- Assets/Scripts/RaycastToggle.cs
+++ Assets/Scripts/RaycastToggle.cs
@@ -9,11 +9,18 @@ public class RaycastToggle : MonoBehaviour
     public InputActionReference triggerAction = null;
     public XRRayInteractor rayInteractor = null;
 
+    public InputActionReference placeMarkerAction = null;
+    public GameObject markerGameObject = null; // Global reference to the markerGameObject Singleton
+
     private void OnEnable()
     {
         if (triggerAction != null)
         {
             triggerAction.action.performed += TriggerPressed;
+        }
+        if (placeMarkerAction != null)
+        {
+            placeMarkerAction.action.performed += PlaceMarker;
         }
     }
 
@@ -23,10 +30,35 @@ public class RaycastToggle : MonoBehaviour
         {
             triggerAction.action.performed -= TriggerPressed;
         }
+        if (placeMarkerAction != null)
+        {
+            placeMarkerAction.action.performed -= PlaceMarker;
+        }
     }
 
     private void TriggerPressed(InputAction.CallbackContext context)
     {
         rayInteractor.enabled = !rayInteractor.enabled;
+    }
+
+    private void PlaceMarker(InputAction.CallbackContext context)
+    {
+        if (rayInteractor.enabled)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(rayInteractor.transform.position, rayInteractor.transform.forward, out hit))
+            {
+                // Move the markerGameObject Singleton to the hit point
+                markerGameObject.transform.position = hit.point;
+
+                // If markerGameObject isn't visible yet, enable it now
+                if (markerGameObject.activeSelf == false)
+                {
+                    markerGameObject.SetActive(true);
+                }
+
+                // TODO: Publish the markerGameObject's position to /goal_pose
+            }
+        }
     }
 }
